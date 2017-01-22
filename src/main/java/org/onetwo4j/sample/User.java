f622830ba843ca2ab3288da3e6d29daff9233bff
wayshall <weishao.zeng@gmail.com>
2017-01-22 19:19:49 +0800

    add boot project for dbm

---
+++ src/main/java/org/onetwo4j/sample/User.java
@@ -0,0 +1,129 @@
+package org.onetwo4j.sample;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.onetwo.dbm.richmodel.RichModel;
+
+@Entity
+@Table(name="web_user")
+public class User extends RichModel<User, Long> {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+	
+    private String userName;
+
+    private String nickName;
+
+    private String password;
+
+    private String email;
+
+    private String mobile;
+
+    private String gender;
+
+    private String status;
+
+    private Date birthday;
+
+    private Date createAt;
+
+    private Date updateAt;
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Date getUpdateAt() {
+		return updateAt;
+	}
+
+	public void setUpdateAt(Date updateAt) {
+		this.updateAt = updateAt;
+	}
+
+}
