7d133ae4c22502524d96ab2ee95327b78ec0cb1d
wayshall <weishao.zeng@gmail.com>
2020-04-29 14:13:55 +0800

    fix to java project

--- src/main/java/org/onetwo4j/sample/DbmSampleApplication.java
+++ src/main/java/org/onetwo4j/sample/DbmSampleApplication.java
@@ -1,8 +1,15 @@
 package org.onetwo4j.sample;
 
+import javax.annotation.PostConstruct;
+
 import org.onetwo.common.db.spi.BaseEntityManager;
 import org.onetwo.common.utils.Page;
 import org.onetwo.dbm.spring.EnableDbm;
+import org.onetwo.ext.apiclient.wechat.EnableWechatClient;
+import org.onetwo.ext.apiclient.wechat.serve.dto.ReceiveMessage.TextMessage;
+import org.onetwo.ext.apiclient.wechat.serve.dto.ReplyMessage.TextReplyMessage;
+import org.onetwo.ext.apiclient.wechat.serve.spi.MessageRouterService;
+import org.onetwo.ext.apiclient.wechat.utils.WechatConstants.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,7 +21,24 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDbm
 @RestController
+@EnableWechatClient(enableOAuth2Interceptor=true)
 public class DbmSampleApplication {
+
+	@Autowired
+	MessageRouterService messageRouterService;
+	
+
+	@PostConstruct
+	public void init(){
+		this.messageRouterService.register(MessageType.TEXT, (TextMessage text)->{
+			return TextReplyMessage.builder()
+									.fromUserName(text.getToUserName())
+									.toUserName(text.getFromUserName())
+									.content("行了，行了，我收到了~")
+									.build();
+		});
+	}
+	
 	@Service
 	@Transactional
 	public static class UserService {
